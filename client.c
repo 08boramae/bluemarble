cc436f840951eb5b7107d6552e6366ac61d482c3
08boramae <mail@0jae0.dev>
2024-11-28 09:35:49 +0900

    interface

--- client.c
+++ client.c
@@ -28,8 +28,8 @@ void drawGameBoard() {
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <winsock2.h>
 #include <windows.h>
+#include <winsock2.h>
 
 #pragma comment(lib, "ws2_32.lib")  // Winsock library
 
